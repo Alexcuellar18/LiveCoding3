5b255b31a5c316cc51f2419adf0c7e0b7d16e902
Alex C <alejandrocuellar18@gmail.com>
2019-12-03 19:12:37 -0500

    live coding

--- LiveCoding3/Program.cs
+++ LiveCoding3/Program.cs
@@ -9,6 +9,17 @@ namespace LiveCoding3
             Console.WriteLine("Hello World!");
         }
 
+        int[] array1 = { 1, 5, 2 };
+        int[] array2 = { 3, 6, 4 };
+        public static Array zipArray(Array first, Array second)
+        {
+            int[] array3 = new int[5];
+
+            Array.Copy(first, 0,);
+
+
+        }
+        
 
     }
 }
